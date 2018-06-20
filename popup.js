7e8ebabb1118da7fac829ed54e16f712acdf6144
Prashant Upadhyay <prashant10723@gmail.com>
2018-06-20 11:49:31 +0530

    Minor changes in JS

--- popup.js
+++ popup.js
@@ -1,7 +1,18 @@
 $(function(){
 	$('#submit').click(function(){
-		var corpus = $('#corpus').val().toString();
-		$('#analysis').text(corpus);
+		var corpuss = $('#corpus').val().toString();
+		var myurl = 'http://127.0.0.1:8000/?corpus='+corpuss;
+		console.log(corpuss);
+		$.ajax({
+			url:myurl,
+			type:'GET',
+			dataType: 'json',
+			crossDomain: true,
+			success: function(data){
+				$('#analysis').text(data.res);				
+			}
+		});
+	
 		
-	})
+	});
 });
